9589f057e756b278a32c0be3b8ffab2c5912a202
Nick Duncan <nick.b.duncan@gmail.com>
2015-07-12 00:35:56 -0700

    added db

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
+//console.log(process.env.DATABASE_URL);
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM test_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+}); 
 
 module.exports = router;
